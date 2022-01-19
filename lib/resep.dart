93ab1a2de88b22375bd92a7d300e16870538b28d
fourman lubis <fourmanlubis99@gmail.com>
2022-01-19 09:37:29 +0700

    commit kedua

--- lib/resep.dart
+++ lib/resep.dart
@@ -20,94 +20,18 @@ List resepList = [
       id: "candilketan",
       makanan: "Candil Ketan",
       alatdanbahan:
-                 "Bahan :- 200 gr tepung ketan.- 30 g tepung tapioka. Tepung beras atau boleh tepung ketan saja.- 1/3 sdt garam.- secukupnya air hangat sampai adonan bisa dipulung.Bahan kuah :- 150 gula merah, disisir.- 800 ml air. - 2 lembar daun pandan ditali simpul.Bahan larutan pengental :- 1 sdm tepung ketan atau boleh lebih jika suka kuah yang kental.- 1 sdm tepung beras atau boleh lebih jika suka kuah yang kental.- Campur semua bahan dan tambahkan sedikit air, aduk hingga larut dan tidak ada yang bergerindil.Bahan kuah santan :- 400 ml santan kental.- 1/4 sdt garam.- Secukupnya buah nangka dipotong kotak kecil2 ( opsional).Rebus kuah santan hingga mendidih bersama garam dan daun pandan. Matikan api. Masukan potongan nangka.Cara memasak : a. Campur tepung ketan, tepung tapioka dan garam, tuangi air hangat secukupnya sampai adonan lembut dan enak buat dipulung ( kalau terlalu lembek boleh ditambahi tepung ketan lagi).b.Siapkan nampan, taburi tipis2 dengan tepung kanji.c.Ambil secuil adonan bentuk bulat sebesar kelereng. Susun di atas nampan. Lakukan sampai adonan habisd. Masak semua bahan kuah kuah gula merah hingga gula larut dan mendidih.e. Saring lalu masak lagi sampai mendidih.f. Masukkan bola bola ketan. Masak dengan api kecil hingga gula meresap dan warna bola bola ketan berubah kecoklatan.g. Masukkan larutan tepung , aduk pelan sampai kental dan meletup2.h. Jika kurang kental bisa ditambahkan tepung ketan/ tepung beras yang dicairkan lalu dimasak lagi sampai kental.i. Sajikan dengan kuah santan.6. Pempek PalembangBahan untuk biang : - 400 ml air.- 200 gr terigu.- Secukupnya Garam dan gula.
-                  Bahan untuk adonan pempek :
-                  - 3 butir telur dikocok (dicampur ke dalam adonan pempek).
-                  - 5 siung bawang putih dihaluskan.
-                  - 400 gr sagu tani.
-                  Bahan untuk isian :
-                  - 2 butir telur dikocok beri garam, gula sedikit.
-                  Cara memasak :
-                  a. Dalam panci campur terigu, air, garam dan gula. Aduk rata. Nyalakan api, masak sambil diaduk-aduk sampai menggumpal seperti bubur (hati-hati gosong) Angkat. Biarkan hangat.
-                  b. Pindahkan biang yang sudah tidak panas ke dalam baskom. Dalam keadaan hangat tambahkan bawang putih, telur kocok. Aduk rata pakai sendok kayu.
-                  c. Masukkan sagu sedikit-sedikit. Aduk rata.
-                  d. Balur tangan dengan sagu, ambil sebagian adonan bentuk silinder
-                  e.Buat lubang dengan menggunakan jari telunjuk. Bentuk seperti contong kemudian isi dengan telur kocok
-                  f. Tutup adonan pempek rapat-rapat supaya tidak terbuka pada saat direbus
-                  g. Rebus pempek dalam air mendidih sampai matang, angkat."),
+                 "Bahan :- 200 gr tepung ketan.- 30 g tepung tapioka. Tepung beras atau boleh tepung ketan saja.- 1/3 sdt garam.- secukupnya air hangat sampai adonan bisa dipulung.Bahan kuah :- 150 gula merah, disisir.- 800 ml air. - 2 lembar daun pandan ditali simpul.Bahan larutan pengental :- 1 sdm tepung ketan atau boleh lebih jika suka kuah yang kental.- 1 sdm tepung beras atau boleh lebih jika suka kuah yang kental.- Campur semua bahan dan tambahkan sedikit air, aduk hingga larut dan tidak ada yang bergerindil.Bahan kuah santan :- 400 ml santan kental.- 1/4 sdt garam.- Secukupnya buah nangka dipotong kotak kecil2 ( opsional).Rebus kuah santan hingga mendidih bersama garam dan daun pandan. Matikan api. Masukan potongan nangka.Cara memasak : a. Campur tepung ketan, tepung tapioka dan garam, tuangi air hangat secukupnya sampai adonan lembut dan enak buat dipulung ( kalau terlalu lembek boleh ditambahi tepung ketan lagi).b.Siapkan nampan, taburi tipis2 dengan tepung kanji.c.Ambil secuil adonan bentuk bulat sebesar kelereng. Susun di atas nampan. Lakukan sampai adonan habisd. Masak semua bahan kuah kuah gula merah hingga gula larut dan mendidih.e. Saring lalu masak lagi sampai mendidih.f. Masukkan bola bola ketan. Masak dengan api kecil hingga gula meresap dan warna bola bola ketan berubah kecoklatan.g. Masukkan larutan tepung , aduk pelan sampai kental dan meletup2.h. Jika kurang kental bisa ditambahkan tepung ketan/ tepung beras yang dicairkan lalu dimasak lagi sampai kental.i. Sajikan dengan kuah santan.6. Pempek PalembangBahan untuk biang : - 400 ml air.- 200 gr terigu.- Secukupnya Garam dan gula.Bahan untuk adonan pempek-3.butir telur dikocok dicampur ke dalam adonan pempek.- 5 siung bawang putih dihaluskan.- 400 gr sagu tani.Bahan untuk isian :- 2 butir telur dikocok beri garam, gula sedikitCara memasak :a. Dalam panci campur terigu, air, garam dan gula. Aduk rata. Nyalakan api, masak sambil diaduk-aduk sampai menggumpal seperti bubur (hati-hati gosong) Angkat. Biarkan hangat.b. Pindahkan biang yang sudah tidak panas ke dalam baskom. Dalam keadaan hangat tambahkan bawang putih, telur kocok. Aduk rata pakai sendok kayu.c. Masukkan sagu sedikit-sedikit. Aduk rata.d. Balur tangan dengan sagu, ambil sebagian adonan bentuk silindere.Buat lubang dengan menggunakan jari telunjuk. Bentuk seperti contong kemudian isi dengan telur kocokf. Tutup adonan pempek rapat-rapat supaya tidak terbuka pada saat direbusg. Rebus pempek dalam air mendidih sampai matang, angkat."),
+  
   resep(
       id: "nasibakepor",
       makanan: "Nasi Bakepor Kalimantan Timur",
       alatdanbahan:
-          "Bahan :
-          - 300 gr beras putih, cuci bersih
-          - 2 lembar daun salam
-          - 1 lembar daun pandan, potong
-          - 4 sdm minyak sayur
-          - 1 sdt garam
-          - 400 ml air
-          - 75 gr ikan asin gabus, goreng, lalu suwir - suwir
-          - 15 lembar daun kemangi
-          - 3 buah cabe rawit, iris bulat tipis
-          1/2 buah jeruk nipis, ambil airnya
-          Sambal Raja :
-          - 1 buah terong, potong dadu 1 cm
-          - 10 lonjor kacang panjang, potong 2 cm
-          - 10 siung bawang merah, belah 4
-          Bumbu halus :
-          - 10 buah cabe rawit
-          - 10 buah cabe merah besar, buang biji
-          - 2 buah jeruk limau
-          - 1 bks kecil terasi
-          - 1 sdt garam
-          - 1 sdm gula pasir
-          Cara memasak :
-          a. Goreng ikan asin gabus. Angkat. Tiriskan. Suwir - suwir. Sisihkan
-          b. Nasi Bakepor : Siapkan panci tebal. Masukkan ke dalamnya beras, air, minyak, daun salam, daun pandan, dan garam.
-          c. Masak di atas api kecil sambil diaduk. Angkat.
-          d. Masukkan daun kemangi, ikan asin suwir, cabai, dan air jeruk nipis, aduk rata.
-          e. Tutup panci kemudian Masak dengan api sangat kecil hingga tanak dan nasi matang.
-          f. Buka tutup panci biarkan hingga permukaan nasi tampak kering dan bagian pinggirnya berkerak. Matikan api.
-          g. Sambal Raja : Goreng kacang panjang, terong dan bawang merah sebentar dengan minyak panas.Tiriskan.
-          h. Ulek cabe rawit, cabe merah, terasi, garam dan gula pasir. Beri perasan jeruk limau. (Kulit jeruk jangan di buang, iris halus kulit jeruk).
-          i. Tumis sambal hingga harum, masukkan kacang panjang, terong dan bawang merah yang telah di goreng tadi.
-          j. Aduk rata sebelum di angkat masukkan irisan kulit jeruk limau. Aduk rata. Matikan api.
-          k. Sambal Raja siap di dampingkan dengan nasi bakepor
-          h. Penyajian : Hidangkan Nasi Bakepor hangat hangat dengan sambal Raja."),
+          "Bahan :-300 gr beras putih, cuci bersih- 2 lembar daun salam- 1 lembar daun pandan, poton- 4 sdm minyak sayur- 1 sdt garam- 400 ml air- 75 gr ikan asin gabus, goreng, lalu suwir - suwir- 15 lembar daun kemangi- 3 buah cabe rawit, iris bulat tipis.-1/2 buah jeruk nipis, ambil airnyaSambal Raja :- 1 buah terong, potong dadu 1 cm- 10 lonjor kacang panjang, potong 2 cm- 10 siung bawang merah, belah 4Bumbu halus :- 10 buah cabe rawit- 10 buah cabe merah besar, buang biji- 2 buah jeruk limau- 1 bks kecil terasi- 1 sdt garam- 1 sdm gula pasirCara memasak :a. Goreng ikan asin gabus. Angkat. Tiriskan. Suwir - suwir. Sisihkanb. Nasi Bakepor : Siapkan panci tebal. Masukkan ke dalamnya beras, air, minyak, daun salam, daun pandan, dan garam.c. Masak di atas api kecil sambil diaduk. Angkat.d. Masukkan daun kemangi, ikan asin suwir, cabai, dan air jeruk nipis, aduk rata.e. Tutup panci kemudian Masak dengan api sangat kecil hingga tanak dan nasi matang.f. Buka tutup panci biarkan hingga permukaan nasi tampak kering dan bagian pinggirnya berkerak. Matikan api.g. Sambal Raja : Goreng kacang panjang, terong dan bawang merah sebentar dengan minyak panas.Tiriskan.h. Ulek cabe rawit, cabe merah, terasi, garam dan gula pasir. Beri perasan jeruk limau.(Kulit jeruk jangan di buang, iris halus kulit jeruk).i. Tumis sambal hingga harum, masukkan kacang panjang, terong dan bawang merah yang telah di goreng tadi.j. Aduk rata sebelum di angkat masukkan irisan kulit jeruk limau. Aduk rata. Matikan api.k. Sambal Raja siap di dampingkan dengan nasi bakeporh. Penyajian : Hidangkan Nasi Bakepor hangat hangat dengan sambal Raja."),
   resep(
       id: "rawonsurabaya",
       makanan: "Rawon Surabaya",
       alatdanbahan:
-          "Bahan:
-          - 500 gram daging sapi, bagian sandung lumur
-          - Air untuk merebus daging
-          - 1.500 ml air kaldu sapi
-          - 3 sdm minyak goreng, untuk menumis
-          - 1 sdm garam
-          Haluskan:
-          - 6 siung bawang putih
-          - 4 biji keluak (ambil isinya, lalu rendam dalam air)
-          - 8 buah cabai merah
-          - 2 ibu jari lengkuas
-          - 3 cm kunyit
-          - 2 cm kencur
-          - 2 cm jahe
-          - 2 sdm ketumbar
-          - 1/4 sdt jintan
-          - 1/2 sdt merica butir
-          - 4 lembar daun salam
-          - 2 batang serai, memarkan
-          - 2 mata asam jawa, larutkan dengan 1 sdm air
-          - 5 lembar daun jeruk purut
-          Pelengkap:
-          - Tauge pendek/kecambah
-          - Telur bebek asin
-          - Sambal terasi
-          - Kerupuk ikan
-          Cara memasak :
-          a. Cuci daging sampai bersih, rebus sampai matang. Angkat dan tiriskan daging, lalu potong ukuran dadu. Sisihkan daging dan air kaldunya.
-          b. Panaskan minyak, tumis bumbu halus sampai harum dan matang. Masukkan daging, serai, daun jeruk, air asam, dan garam. Aduk sampai rata dan beraroma.
-          c. Pukul-pukul daging sampai melebar. Goreng dalam minyak panas sebentar, angkat. Taburi dengan bawang goreng"),
+          "Bahan:- 500 gram daging sapi, bagian sandung lumur- Air untuk merebus daging- 1.500 ml air kaldu sapi- 3 sdm minyak goreng, untuk menumis- 1 sdm garamHaluskan:- 6 siung bawang putih- 4 biji keluak (ambil isinya, lalu rendam dalam air)- 8 buah cabai merah- 2 ibu jari lengkuas- 3 cm kunyit- 2 cm kencur- 2 cm jahe- 2 sdm ketumbar- 1/4 sdt jintan- 1/2 sdt merica butir- 4 lembar daun salam- 2 batang serai, memarkan- 2 mata asam jawa, larutkan dengan 1 sdm air- 5 lembar daun jeruk purutPelengkap:- Tauge pendek/kecambah- Telur bebek asin- Sambal terasi- Kerupuk ikanCara memasak :a. Cuci daging sampai bersih, rebus sampai matang. Angkat dan tiriskan daging, lalu potong ukuran dadu. Sisihkan daging dan air kaldunya.b. Panaskan minyak, tumis bumbu halus sampai harum dan matang. Masukkan daging, serai, daun jeruk, air asam, dan garam. Aduk sampai rata dan beraroma. c. Pukul-pukul daging sampai melebar. Goreng dalam minyak panas sebentar, angkat. Taburi dengan bawang goreng"),
   resep(
       id: "seblakbandung",
       makanan: "Seblak Bandung",
